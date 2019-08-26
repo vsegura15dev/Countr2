d9b7c09b87b0e8dea54866917c5f068ce4951982
Victor Segura <vsegura15@gmail.com>
2019-08-26 00:35:05 -0500

    done

--- Countr2.iOS/AppDelegate.cs
+++ Countr2.iOS/AppDelegate.cs
@@ -18,8 +18,12 @@ namespace Countr2.iOS
 
         public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
         {
-
-            Window = new UIWindow(UIScreen.MainScreen.Bounds);
+			UINavigationBar.Appearance.BarTintColor = UIColor.Orange;
+			UINavigationBar.Appearance.TintColor = UIColor.DarkGray;
+			var attrs = new NSDictionary(UIStringAttributeKey.ForegroundColor,
+										 UIColor.DarkGray);
+			UINavigationBar.Appearance.TitleTextAttributes = new UIStringAttributes(attrs);
+			Window = new UIWindow(UIScreen.MainScreen.Bounds);
             Window.MakeKeyAndVisible();
             return base.FinishedLaunching(application, launchOptions);
         }
